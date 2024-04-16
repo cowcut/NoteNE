23bc8afddaa57037f6a64b78a5d5fd64158242aa
danau <zacerorido@gmail.com>
2024-04-16 12:36:29 +0700

    add gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,15 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.room:room-runtime:2.5.0")
+    implementation("androidx.room:room-compiler:2.5.0")
+
+    //RecycleView
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    //Meterial Size
+    implementation("com.google.android.material:material:1.11.0")
+
+    //ImageView
+    implementation("com.android.support:design-widget:28.0.0-alpha1")
 }
