7c833329a0244c49768415a8535396a96dfa021b
cowcut <112707238+cowcut@users.noreply.github.com>
2024-04-22 09:59:07 +0700

    improve

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,28 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        // The classpath for the plugin is added here
+        classpath("com.android.tools.build:gradle:8.2.0")
+    }
+}
+
 plugins {
     id("com.android.application") version "8.2.0" apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
