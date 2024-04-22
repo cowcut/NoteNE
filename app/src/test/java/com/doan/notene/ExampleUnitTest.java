da0991d09775c2ac4523502e9dab3d1364ecbb6b
cowcut <112707238+cowcut@users.noreply.github.com>
2024-04-22 09:59:08 +0700

    improve

--- app/src/test/java/com/doan/notene/ExampleUnitTest.java
+++ app/src/test/java/com/doan/notene/ExampleUnitTest.java
@@ -10,8 +10,39 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void subtraction_isCorrect() {
+        assertEquals(0, 2 - 2);
+    }
+
+    @Test
+    public void multiplication_isCorrect() {
+        assertEquals(6, 2 * 3);
+    }
+
+    @Test
+    public void division_isCorrect() {
+        assertEquals(2, 6 / 3d);
+    }
+
+    @Test
+    public void modulus_isCorrect() {
+        assertEquals(1, 5 % 2);
+    }
+
+    @Test
+    public void negativeNumbers_isCorrect() {
+        assertEquals(-2, -1 - 1);
+    }
+
+    @Test
+    public void zero_isCorrect() {
+        assertEquals(0, 0 + 0);
     }
 }
