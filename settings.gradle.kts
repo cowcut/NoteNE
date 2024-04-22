e8ed63ae142fa0157f93179e9380cb797854b06b
cowcut <112707238+cowcut@users.noreply.github.com>
2024-04-22 09:59:08 +0700

    improve

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,24 @@
+plugins {
+    id 'com.android.application' version '7.1.2' apply false
+    id 'com.android.library' version '7.1.2' apply false
+    id 'org.jetbrains.kotlin.android' version '1.6.10' apply false
+}
+
 pluginManagement {
     repositories {
-        google()
-        mavenCentral()
         gradlePluginPortal()
+        mavenCentral()
+        google()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        google()
         mavenCentral()
+        google()
     }
 }
 
 rootProject.name = "NoteNe"
 include(":app")
- 
